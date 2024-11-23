5d9be3166a8c88a4ce8aba15b0cdcc67a0726579
abvictor <victor.amaralb@hotmail.com>
2024-11-23 17:09:05 -0300

    EnvConfig unit tests

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -7,7 +7,7 @@ export class EnvConfigService implements EnvConfig {
   constructor(private configService: ConfigService) {}
 
   getAppPort(): number {
-    return Number(this.configService.get('PORT'));
+    return Number(this.configService.get<number>('PORT'));
   }
 
   getNodeEnv(): string {
